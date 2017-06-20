00a75596d48b08de1fbe81e7ec8f05d850e53baa
chenzhipei <chenzhipei@hotoa.com>
2017-06-20 15:27:21 +0800

    添加淘宝源

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ npm install -g node-gyp
 meteor build --server https://cn.steedos.com/records --directory C:/records-build/
 cd C:/records-build/bundle/programs/server
 rm -rf node_modules
-npm install
+npm install --registry https://registry.npm.taobao.org -d
 
 cd ../../
 node main.js
