c0d5d860e9374092f97442521ddd9a13c15e178e
chenzhipei <chenzhipei@hotoa.com>
2017-06-20 15:44:06 +0800

    修改build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,3 @@
-npm install --global --production windows-build-tools
-npm install -g node-gyp
-
 meteor build --server https://cn.steedos.com/records --directory C:/records-build/
 cd C:/records-build/bundle/programs/server
 rm -rf node_modules
