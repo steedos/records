78ba8b5128bbc8316c6f612c0f16104520fac202
zonglu <415390588@qq.com>
2017-06-20 10:18:19 +0800

    更换为秦皇岛数据库

--- start.bat
+++ start.bat
@@ -1,8 +1,9 @@
-set DB_SERVER=192.168.0.21
+rem set DB_SERVER=127.0.0.1
+set DB_SERVER=192.1.1.141
 set MONGO_URL=mongodb://%DB_SERVER%/steedos
 set MONGO_OPLOG_URL=mongodb://%DB_SERVER%/local
 set MULTIPLE_INSTANCES_COLLECTION_NAME=workflow_instances
-set ROOT_URL=http://localhost:3000/
+set ROOT_URL=http://127.0.0.1:3000/
 rem set MAIL_URL=smtp://noreply@message.steedos.com:lw3YEbNUbmkoSJDuAAAb@smtpdm.aliyun.com:25
 meteor run --settings settings.json 
 rem--port 3007
