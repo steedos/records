9fc6cbc325abbe36e7070eb21f2c436128c548b0
zonglu <415390588@qq.com>
2017-07-31 11:38:00 +0800

    表单url的参数设置

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
-set DB_SERVER=127.0.0.1
-rem set DB_SERVER=192.1.1.141
+rem set DB_SERVER=127.0.0.1
+set DB_SERVER=192.1.1.141
 set MONGO_URL=mongodb://%DB_SERVER%/steedos
 set MONGO_OPLOG_URL=mongodb://%DB_SERVER%/local
 set MULTIPLE_INSTANCES_COLLECTION_NAME=workflow_instances
-set ROOT_URL=http://127.0.0.1:3000/records
+set ROOT_URL=http://127.0.0.1:3006/records
 rem set MAIL_URL=smtp://noreply@message.steedos.com:lw3YEbNUbmkoSJDuAAAb@smtpdm.aliyun.com:25
 meteor run --settings settings.json 
 rem--port 3007
