40920abd8e2ef09290e8ce603f6994357cc6c331
zonglu <415390588@qq.com>
2017-08-11 18:56:03 +0800

    start.bat启动数据库

--- start.bat
+++ start.bat
@@ -1,7 +1,10 @@
-set DB_SERVER=127.0.0.1
-rem set DB_SERVER=192.1.1.141
+rem set DB_SERVER=127.0.0.1
+set DB_SERVER=192.1.1.141
+rem set DB_SERVER=192.1.1.215,192.1.1.216,192.1.1.217
 set MONGO_URL=mongodb://%DB_SERVER%/steedos
+rem set MONGO_URL=mongodb://dbnormal:oaoper@%DB_SERVER%/steedos?replicaSet=steedos
 set MONGO_OPLOG_URL=mongodb://%DB_SERVER%/local
+rem set MONGO_OPLOG_URL=mongodb://dbnormal:oaoper%DB_SERVER%/local?replicaSet=steedos&authSource=steedos
 set MULTIPLE_INSTANCES_COLLECTION_NAME=workflow_instances
 set ROOT_URL=http://127.0.0.1:3000/records
 rem set MAIL_URL=smtp://noreply@message.steedos.com:lw3YEbNUbmkoSJDuAAAb@smtpdm.aliyun.com:25
